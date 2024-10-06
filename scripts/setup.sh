ba6d761e62b43b42242af943dc39a8de79c7bcae
JSQ <j996730508@163.com>
2024-10-06 16:26:22 +0800

    ci: try github action

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 
-cd /
-cd /software/jsonq.top/server
+WORK_DIR="/software/jsonq.top/server"
+if [ ! -d "$WORK_DIR" ]; then
+  echo "Directory $WORK_DIR does not exist. Please check the path."
+  exit 1
+fi
+cd "$WORK_DIR"
 
-# check git installed
-# if ! command -v git &> /dev/null
-# then
-#     echo "git not found, installing..."
-#     if command -v yum &> /dev/null
-#     then
-#         sudo yum install -y git
-#     else
-#         echo "no supported package manager, please install git manually."
-#         exit 1
-#     fi
-# fi
+if ! command -v git &> /dev/null; then
+  echo "git not found, installing..."
+  if command -v yum &> /dev/null; then
+    sudo yum install -y git
+  else
+    echo "No supported package manager, please install git manually."
+    exit 1
+  fi
+fi
 
-# # set git config
 # git config --global user.name "JSQ"
 # git config --global user.email "j996730508@163.com"
 
 git pull origin server
 
-# node install
-# see: https://github.com/nodesource/distributions?tab=readme-ov-file#no-root-privileges-nodejs-20
+if ! command -v node &> /dev/null; then
+  echo "node not found, please install Node.js manually."
+  exit 1
+fi
 
-# npm install --production
-npm install --omit=dev
+npm install --production
 
 npm run build
 
-check pm2 installed
-if ! command -v pm2 &> /dev/null
-then
-    echo "pm2 not found, installing..."
-    npm install -g pm2
+if ! command -v pm2 &> /dev/null; then
+  echo "pm2 not found, installing..."
+  npm install -g pm2
 fi
 
 APP_NAME="nest-oss"
 
-pm2 restart $APP_NAME  || pm2 start dist/src/main.js --name $APP_NAME
+pm2 restart $APP_NAME || pm2 start dist/src/main.js --name $APP_NAME
